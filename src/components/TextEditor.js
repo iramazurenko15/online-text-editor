f8ec5bb71f4dd458b178881b8f10aa9c92f961fa
Iryna Mazurenko <mazurenko.iryna@gmail.com>
2019-07-15 15:23:36 +0300

    add h1 and h2 buttons

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -5,8 +5,18 @@ import { ToolbarButton } from './index';
 import { bold } from 'react-icons-kit/feather/bold';
 import { italic } from 'react-icons-kit/feather/italic';
 import { underline } from 'react-icons-kit/feather/underline';
+import { u0031 } from 'react-icons-kit/noto_emoji_regular/u0031';
+import { u0032 } from 'react-icons-kit/noto_emoji_regular/u0032';
 import { code } from 'react-icons-kit/feather/code';
 import { list } from 'react-icons-kit/feather/list';
+
+
+import { isKeyHotkey } from 'is-hotkey'
+
+const DEFAULT_NODE = 'paragraph';
+const isBoldHotkey = isKeyHotkey('mod+b')
+const isItalicHotkey = isKeyHotkey('mod+i')
+const isUnderlinedHotkey = isKeyHotkey('mod+u')
 
 
 const initialValue = Value.fromJSON({
@@ -31,7 +41,8 @@ export default class TextEditor extends Component {
     state = {
         value: initialValue,
         iconsArr: [{ type: 'bold', icon: bold }, { type: 'italic', icon: italic }, { type: 'underline', icon: underline },
-        { type: 'list', icon: list }, { type: 'code', icon: code }]
+        { type: 'heading-one', icon: u0031 }, { type: 'heading-two', icon: u0032 },
+        { type: 'bulleted-list', icon: list }, { type: 'code', icon: code }]
     }
 
     hasMark = type => {
@@ -39,43 +50,73 @@ export default class TextEditor extends Component {
         return value.activeMarks.some(mark => mark.type === type)
     }
 
+    hasBlock = type => {
+        const { value } = this.state
+        return value.blocks.some(node => node.type === type)
+    }
+
     onChange = ({ value }) => {
         this.setState({ value })
     }
 
-    onKeyDown = (e, editor) => {
-        console.log(editor, 'editor');
-
-        if (!e.ctrlKey) { return };
-
-        switch (e.key) {
-            case 'b': {
-                e.preventDefault();
-                editor.toggleMark('bold')
-                return true
-            }
-            case 'i': {
-                e.preventDefault();
-                editor.toggleMark('italic')
-                return true
-            }
-            case 'u': {
-                e.preventDefault();
-                editor.toggleMark('underline')
-                return true
-            }
-            case 'l': {
-                e.preventDefault();
-                editor.toggleMark('list')
-                return true
-            }
+    onKeyDown = (e, editor, next) => {
+        if (isBoldHotkey(e)) {
+            e.preventDefault();
+            editor.toggleMark('bold')
         }
+        else if (isItalicHotkey(e)) {
+            e.preventDefault();
+            editor.toggleMark('italic')
+        }
+        else if (isUnderlinedHotkey(e)) {
+            e.preventDefault();
+            editor.toggleMark('underline')
+        }
+        else {
+            return next()
+        }
+
     }
 
-    onMarkClick = (e, type) => {
-        e.preventDefault();
-        this.editor.toggleMark(type);
+    onButtonClick = (e, type) => {
 
+        e.preventDefault();
+        const { editor } = this
+        const { value } = editor
+        const { document } = value
+        const isActive = this.hasBlock(type);
+        if (type === 'bulleted-list') {
+            const isList = this.hasBlock('list-item');
+            console.log(isList, '---isList')
+            const isType = value.blocks.some(block => {
+                return !!document.getClosest(block.key, parent => parent.type === type)
+            })
+
+            console.log(isType, '--isType');
+
+            if (isList && isType) {
+                console.log('1');
+                editor
+                    .setBlocks(DEFAULT_NODE)
+                    .unwrapBlock('bulleted-list')
+            } else if (isList) {
+                console.log('2');
+                editor
+                    .unwrapBlock('bulleted-list')
+                    .wrapBlock(type)
+            } else {
+                console.log('3');
+                editor.setBlocks('list-item').wrapBlock(type)
+            }
+        }
+
+        else if (type === "heading-one" || type === "heading-two") {
+            editor.setBlocks(isActive ? DEFAULT_NODE : type)
+
+        }
+        else {
+            editor.toggleMark(type)
+        }
     }
 
 
@@ -99,18 +140,30 @@ export default class TextEditor extends Component {
                     }>{props.children}</span >
                 )
             }
-            case 'list': {
-                return (
-                    <ul {...props.attributes}>
-                        <li>{props.children}</li>
-                    </ul>
-                )
-            }
+
             case 'code': {
                 return (
                     <code>{props.children}</code>
                 )
             }
+        }
+    }
+
+    renderBlock = (props, editor, next) => {
+        const { attributes, children, node } = props
+        switch (node.type) {
+            case 'bulleted-list':
+                return <ul {...attributes}>{children}</ul>
+            case 'list-item':
+                console.log('list-item');
+                return <li {...attributes}>{children}</li>
+            case 'heading-one':
+                return <h1 {...attributes}>{children}</h1>
+            case 'heading-two':
+                return <h2 {...attributes}>{children}</h2>
+
+            default:
+                return next()
         }
     }
 
@@ -123,8 +176,10 @@ export default class TextEditor extends Component {
                             <ToolbarButton
                                 type={item.type}
                                 icon={item.icon}
-                                onMarkClick={this.onMarkClick}
+                                onButtonClick={this.onButtonClick}
                                 hasMark={this.hasMark}
+                                hasBlock={this.hasBlock}
+                                value={this.state.value}
                             />
                         )
                     })}
@@ -135,6 +190,7 @@ export default class TextEditor extends Component {
                     onChange={this.onChange}
                     onKeyDown={this.onKeyDown}
                     renderMark={this.renderMark}
+                    renderBlock={this.renderBlock}
                     ref={editor => this.editor = editor}
                 />
             </Fragment>
--- src/components/ToolbarButton.js
+++ src/components/ToolbarButton.js
@@ -3,11 +3,29 @@ import Icon from 'react-icons-kit';
 
 
 const ToolbarButton = (props) => {
-    const isActive = props.hasMark(props.type)
-    console.log(isActive, 'active');
+    let isActive;
+
+    if (props.type === 'heading-one' || props.type === 'heading-two') {
+        isActive = props.hasBlock(props.type);
+    }
+
+    if (props.type === 'bulleted-list') {
+        isActive = props.hasBlock(props.type);
+        const { document, blocks } = props.value;
+
+        if (blocks.size > 0) {
+            console.log(blocks.size, '---blocks.size')
+            const parent = document.getParent(blocks.first().key)
+            isActive = props.hasBlock('list-item') && parent && parent.type === props.type
+        }
+    }
+    else {
+        isActive = props.hasMark(props.type)
+    }
+
     return (
         <button
-            onPointerDown={(e) => props.onMarkClick(e, props.type)}
+            onPointerDown={(e) => props.onButtonClick(e, props.type)}
             className={!isActive ? "tooltip-icon-button" : "tooltip-icon-button active-button"}
         >
             <Icon icon={props.icon} />
